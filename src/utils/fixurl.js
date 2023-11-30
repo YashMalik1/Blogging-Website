ba527e9e1fb25607cfb5a1fdff537057ac612dea
Yash Kumar Malik <yashmalik2711@gmail.com>
2023-12-01 00:14:19 +0530

    fixed

--- src/utils/fixurl.js
+++ src/utils/fixurl.js
@@ -1,8 +1,13 @@
 const httpToHttps = (url) => {
+    console.log(url)
+    if (!url) {
+        return null
+    }
     let newUrl = url.replace('http://', 'https://');
     if (!newUrl.includes('https://')) {
         newUrl = 'https://' + newUrl
     }
+    console.log(newUrl);
     return newUrl
 }
 
